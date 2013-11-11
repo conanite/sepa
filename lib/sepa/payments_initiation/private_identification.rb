e129804868f78adffd2746e9c1a18e9adbbe3ef5
conanite <conan@conandalton.net>
2013-11-11 15:11:40 +0100

    oops, wrong require statement

--- lib/sepa/payments_initiation/private_identification.rb
+++ lib/sepa/payments_initiation/private_identification.rb
@@ -1,4 +1,4 @@
-require "sepa/payments_initiation/generic_organisation_identification_1"
+require "sepa/payments_initiation/generic_person_identification_1"
 
 class Sepa::PaymentsInitiation::PrivateIdentification < Sepa::Base
   definition "Unique and unambiguous identification of a person, eg, passport."
